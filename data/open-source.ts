26300aea94d5869e0d73a7c3d9fd1af9a7a24b57
Samarth Patel <samp231004@gmail.com>
2026-07-11 11:39:48 +0530

    Publish latest portfolio build

--- data/open-source.ts
+++
@@ -1,9 +0,0 @@
-export type OpenSourceContribution = {
-  repo: string;
-  description: string;
-  status: "Merged" | "Open" | "Closed";
-  technologies: string[];
-  prUrl: string;
-};
-
-export const openSourceContributions: OpenSourceContribution[] = [];
